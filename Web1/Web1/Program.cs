3f281b0cd448b071c0d81e7ed878e8044fc9c0c8
ailun001 <43659626+ailun001@users.noreply.github.com>
2022-09-12 02:04:37 +1200

    web1
    before class guess number game
    web 1 work

---
+++ Web1/Web1/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Web1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver webDriver = new ChromeDriver();
+
+            webDriver.Url = "http://www.google.co.nz";
+
+            string title = webDriver.Title;
+            Console.WriteLine("Title is "+ title);
+            string pageSource = webDriver.PageSource;
+            int pageSourceLength = pageSource.Length;
+            Console.WriteLine("length of page source is "+ pageSourceLength);
+
+            webDriver.FindElement(By.LinkText("Gmail")).Click();
+            webDriver.Navigate().Back();
+            webDriver.Navigate().Forward();
+            webDriver.Navigate().Refresh();
+
+            webDriver.Navigate().Back();
+            IWebElement searchInput = webDriver.FindElement(By.Name("q"));
+            searchInput.SendKeys("facebook");
+            searchInput.Clear();
+
+            webDriver.Quit();
+        }
+    }
+}
---
+++ guess/guess/Program.cs
@@ -0,0 +1,69 @@
+﻿
+class A
+{
+    static void Main()
+    {
+        //enter min number
+        Console.WriteLine("enter Min: \n");
+        int min = int.Parse(Console.ReadLine());
+
+        //enter max number
+        Console.WriteLine("enter Max: \n");
+        int max = int.Parse(Console.ReadLine());
+        
+        //setup the number and game
+        A a = new A(min, max);
+        //can guess 5 times
+        int tryTime = 5;
+
+        //game start
+        while (tryTime != 0)
+        {
+            // enter guess number
+            Console.WriteLine("take a guess: ");
+            int guess = int.Parse(Console.ReadLine());
+
+            //guess number is the number win the game and exit
+            if (guess == a.getNum())
+            {
+                Console.WriteLine("Win \n");
+                break;
+            }
+            else
+            {
+                //hint
+                if (guess > a.getNum())
+                {
+                    Console.WriteLine("too high");
+                }
+                else
+                {
+                    Console.WriteLine("too low");
+                }
+            }
+            tryTime--;//times -1
+        }
+
+        //after 5 times, still not get the number lose the game
+        if (tryTime == 0)
+        {
+            Console.WriteLine("lose");
+        }
+    }
+
+    private int min, max, num;
+    
+    public A(int min, int max)
+    {
+        this.min = min;
+        this.max = max;
+        Random random = new Random();
+        this.num = random.Next(min,max);
+    }
+
+    public int getNum()
+    {
+        return num;
+    }
+   
+}
